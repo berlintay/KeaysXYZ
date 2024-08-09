db12459fa7d4755d489ad9b9e9f2f30f7310cfdb
Joshua Taylor Keays <128968711+berlintay@users.noreply.github.com>
2024-08-09 20:12:52 -0300

    Update capture-screenshot.js

--- capture-screenshot.js
+++ capture-screenshot.js
@@ -1,16 +1,19 @@
 const puppeteer = require('puppeteer');
 
-
 async function captureScreenshot(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  
+
+
   await page.goto(url);
 
-  await new Promise(resolve => setTimeout(resolve, 15000));
-  
+
+  await new Promise(resolve => setTimeout(resolve, 15000));  
+
+
   await page.screenshot({ path: 'media/screenshot.png' });
-  
+
+
   await browser.close();
 }
 
